502ce13e17bd8b9443e52b4436526a4352bdda23
Arthur Theodoro | Front End Developer <arthur.theodoro.sva@hotmail.com>
2023-03-30 16:42:53 -0300

    uptade the login function with the new api file

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,34 +1,5 @@
-export function loginCall(userName, password) {
-  const users = [
-    {
-      id: 200,
-      name: "Joao",
-      password: "1234567",
-    },
-    {
-      id: 201,
-      name: "Maria",
-      password: "1234567",
-    },
-    {
-      id: 202,
-      name: "Pedro",
-      password: "1234567",
-    },
-  ];
+import axios from "axios";
 
-  let filteredUser = users.filter((user) => {
-    return user.name == userName;
-  });
-
-  return new Promise((resolve, reject) => {
-    if (
-      userName == filteredUser[0].name &&
-      password == filteredUser[0].password
-    ) {
-      resolve();
-    } else {
-      reject({ message: "usuario ou senha incorretos" });
-    }
-  });
-}
+export const api = axios.create({
+  baseURL: "http://localhost:5000/",
+});
